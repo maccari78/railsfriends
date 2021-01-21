c0677216647a9953e804b629ef8a537b63b94318
Danilo Maccari <maccari78@gmail.com>
2021-01-21 14:17:28 -0300

    Last commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,6 @@ class HomeController < ApplicationController
   end
 
   def about
-    @about_me = "My name is Danilo Maccari and this is my first page on Ruby on Rails"
+    @about_me = "My name is Danilo Maccari and this is my Ruby on Rails FriendsApp"
   end
 end
